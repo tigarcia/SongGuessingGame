57a6d84187a85d7d7382e16a4fa73d78c5c398f5
Tim Garcia <tim.m.garcia@gmail.com>
2017-02-25 15:01:49 -0800

    Styling starter code

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  Button,
+  TextInput,
+  TouchableOpacity,
   StyleSheet,
 } from 'react-native'
 
 export default class Welcome extends Component {
   render() {
     return (
-      <View style={styles.conatiner}>
-        <Text>Hello React-Native</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcomeText}>Hello React-Native</Text>
+        <Button title="SIMPLE BUTTON" onPress={() => null} color="blue"/>
+        <View style={styles.inputContainer}>
+          <TouchableOpacity
+              style={styles.customButton}
+              color="blue">
+            <Text style={styles.buttonText}>PRESS ME</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.customButton}
+              color="blue">
+            <Text style={styles.buttonText}>NO! PRESS ME</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -17,5 +33,19 @@ export default class Welcome extends Component {
 
 const styles = StyleSheet.create({
   container: {
-  }
+    flex: 1,
+  },
+  inputContainer: {
+    width: 350,
+    height: 100,
+  },
+  welcomeText: {
+    fontSize: 30
+  },
+
+  customButton: {
+    borderRadius: 4,
+  },
+  buttonText: {
+  } 
 });
